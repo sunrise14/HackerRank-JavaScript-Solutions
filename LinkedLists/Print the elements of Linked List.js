7d451d97e7e3c59da5cc14727a1613902ffc7ded
rishi <34141382+rishitreddy@users.noreply.github.com>
2018-02-23 19:19:05 -0500

    Initial commit

---
+++ LinkedLists/Print the elements of Linked List.js
@@ -0,0 +1,11 @@
+function print(head) {
+    if(!head){console.log(NULL)}
+        else{
+            while(this.head){
+                this.head.data = this.head;
+                console.log(this.head.data);
+                this.head = this.head.next;
+            }
+            
+        }
+    }
